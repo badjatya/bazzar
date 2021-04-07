a042c0d069e40bc01335849ce7333c5ccbe6c753
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-04-07 13:07:40 +0530

    homepage component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Homepage from "./homepage.component"
 
 function App() {
   return (
     <div className="App">
-      <p>Hello World!</p>
+      <Homepage />
     </div>
   );
 }
