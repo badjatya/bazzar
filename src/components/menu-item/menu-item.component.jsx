9b5fbef6652787abfcf1ce2192ad54bf8ec6c7b8
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-04-08 12:28:00 +0530

    Google fonts

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -10,7 +10,7 @@ function MenuItem({title, imageUrl, size}) {
             }}
             className={`${size} menu-item`}>
             <div className="content">
-                <h1 className="title">{title}</h1>
+                <h1 className="title">{title.toUpperCase()}</h1>
                 <span className="subtitle">SHOP NOW</span>
             </div>
         </div>
