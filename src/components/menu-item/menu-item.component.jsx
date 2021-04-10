c0cf7ee530cd116395fcab97775387d862a65b9b
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-04-10 14:53:45 +0530

    menu item update

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -4,11 +4,12 @@ import "./menu-item.style.scss"
 
 function MenuItem({title, imageUrl, size}) {
     return (
-        <div
-            style={{
-            backgroundImage: `url(${imageUrl})`
-            }}
-            className={`${size} menu-item`}>
+        <div className={`${size} menu-item`}>
+            <div
+                className="background-image"
+                style={{
+                backgroundImage: `url(${imageUrl})`
+            }}/>
             <div className="content">
                 <h1 className="title">{title.toUpperCase()}</h1>
                 <span className="subtitle">SHOP NOW</span>
