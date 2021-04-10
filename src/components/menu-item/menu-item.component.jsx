192bdd7ad883d08c1e831b1919c58e8db6ed2b67
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-04-10 15:06:29 +0530

    hover effect menu item

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -6,13 +6,13 @@ function MenuItem({title, imageUrl, size}) {
     return (
         <div className={`${size} menu-item`}>
             <div
-                className="background-image"
+                className='background-image'
                 style={{
                 backgroundImage: `url(${imageUrl})`
             }}/>
-            <div className="content">
-                <h1 className="title">{title.toUpperCase()}</h1>
-                <span className="subtitle">SHOP NOW</span>
+            <div className='content'>
+                <h1 className='title'>{title.toUpperCase()}</h1>
+                <span className='subtitle'>SHOP NOW</span>
             </div>
         </div>
     )
