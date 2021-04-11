bff6f7c03524dc9d7da66b12a4c9f5144d7efccd
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-04-11 13:44:47 +0530

    Styling Shop's page

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import CollectionItem from '../collection-item/collection-item.component'
+
 import "./collection-preview.styles.scss"
 
 const CollectionPreview = ({title, items}) => (
@@ -7,8 +9,8 @@ const CollectionPreview = ({title, items}) => (
         <h1 className="title">{title.toUpperCase()}</h1>
         <div className="preview">
             {
-                items.filter((item,index) => index < 4).map(item => (
-                    <div key={item.id}>{item.name}</div>
+                items.filter((item,index) => index < 4).map(({id , ...itemOtherProps}) => (
+                    <CollectionItem key={id} {...itemOtherProps} />
                 ))
             }
         </div>
