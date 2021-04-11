83b00d183a673c15d908d99439a85328ff119870
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-04-12 01:08:57 +0530

    Sign In Component and Sign In Form

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -6,13 +6,50 @@ class SignIn extends React.Component {
 
         this.state = {
             email: '',
-            password: '',
+            password: ''
         }
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+
+        this.setState({email: '', password: ''})
+    }
+
+    handleChange = event => {
+        const {value, name} = event.target;
+
+        this.setState({[name]: value})
     }
 
     render() {
         return (
-            
+            <div className="sign-in">
+                <h2>I Already have an account</h2>
+                <span>Sign in with your email and password.</span>
+
+                <form onSubmit={this.handleSubmit}>
+                    <input
+                        type="email"
+                        name="email"
+                        id="email"
+                        value={this.state.email}
+                        onChange={this.handleChange}
+                        required/>
+                    <label>Email</label>
+
+                    <input
+                        type="password"
+                        name="password"
+                        id="password"
+                        value={this.state.password}
+                        onChange={this.handleChange}
+                        required/>
+                    <label>Password</label>
+
+                    <input type="submit" value="Submit"/>
+                </form>
+            </div>
         )
     }
 }
