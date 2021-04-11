cf1620f23c5596de31fdebcef8aa6754e050fe1d
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-04-12 00:51:59 +0530

    Sign In Component

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+class SignIn extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: '',
+            password: '',
+        }
+    }
+
+    render() {
+        return (
+            
+        )
+    }
+}
+
+export default SignIn;
