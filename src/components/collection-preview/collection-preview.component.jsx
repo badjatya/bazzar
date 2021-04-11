b25914937366d90df20a2620200a07a5a86b60a5
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-04-11 13:32:41 +0530

    collection-preview styles

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import "./collection-preview.styles.scss"
+
 const CollectionPreview = ({title, items}) => (
     <div className="collection-preview" >
         <h1 className="title">{title.toUpperCase()}</h1>
