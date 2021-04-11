f90e88b6578468ba9a8eb5e012af813b706dc673
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-04-11 13:35:14 +0530

    collection-item styles

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import "./collection-item.styles.scss"
+
 const CollectionItem = ({id, name, price, imageUrl}) => (
     <div className="collection-item">
         <div 
