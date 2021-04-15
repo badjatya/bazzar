b913ced665892adf192e3781b905e51c01d5d207
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-04-15 15:08:30 +0530

    SIgnIn Component Buttons class

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -53,8 +53,10 @@ class SignIn extends React.Component {
                         label="Password"
                         required/>
 
-                    <CustomButton type="submit">Sign in</CustomButton>
-                    <CustomButton onClick={signInWithGoogle} igGoogleSignIn>Sign in with Google</CustomButton>
+                    <div className="buttons">
+                        <CustomButton type="submit">Sign in</CustomButton>
+                        <CustomButton onClick={signInWithGoogle} igGoogleSignIn>Sign in with Google</CustomButton>
+                    </div>
                 </form>
             </div>
         )
