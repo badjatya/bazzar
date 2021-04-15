41eab789092b220c29c84c1556dc5d9efc6348c4
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-04-15 13:53:07 +0530

    App State

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import React from 'react'
 import {Switch, Route} from "react-router-dom"
 
 import Homepage from "./pages/homepage/homepage.component"
@@ -6,18 +6,28 @@ import ShopPage from "./pages/shop/shop.component"
 import Header from "./components/header/header.component"
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component"
 
+import './App.css';
 
-const App = () => {
-  return (
-    <div className="App">
-      <Header />
-      <Switch >
-        <Route exact path="/" component={Homepage} />
-        <Route exact path="/shop" component={ShopPage} />
-        <Route exact path="/signin" component={SignInAndSignUpPage} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(){
+    super();
+
+    
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Switch >
+          <Route exact path="/" component={Homepage} />
+          <Route exact path="/shop" component={ShopPage} />
+          <Route exact path="/signin" component={SignInAndSignUpPage} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
