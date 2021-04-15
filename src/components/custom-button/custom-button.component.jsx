4ed258b961dff35432a66067ab23e003a47ea06b
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-04-15 15:01:51 +0530

    Custom button container update

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 import "./custom-button.styles.scss"
 
-const CustomButton = ({children, ...otherProps}) => (
-    <button className="custom-button" {...otherProps}>
+const CustomButton = ({children, igGoogleSignIn, ...otherProps}) => (
+    <button className={`${igGoogleSignIn ? 'google-sign-in' : ''} 'custom-button'`} {...otherProps}>
         {children}
     </button>
 )
