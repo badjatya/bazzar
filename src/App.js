6b9678ee05b5388cb7aecc109cd63ef704bcbc7d
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-04-15 17:33:37 +0530

    Storing data from firebase in app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Header from "./components/header/header.component"
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component"
 
 import './App.css';
-import { auth } from './firebase/firebase.utils'
+import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 
 class App extends React.Component {
 
@@ -22,8 +22,23 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount(){
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser : user})
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if(userAuth){
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapshot => {
+          this.setState({
+            currentUser : {
+              id: snapshot.id,
+              ...snapshot.data(),
+            }
+          }, () => {
+            console.log(this.state);
+          })
+        })
+      }
+
+      this.setState({currentUser : userAuth})
     } )
   }
 
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -15,7 +15,27 @@ const config = {
 export const createUserProfileDocument = async (userAuth, additionalData) => {
     if(!userAuth) return;
 
-    console.log(firestore.doc('users/123ej92s'));
+    const userRef = firestore.doc(`users/${userAuth.uid}`)
+
+    const snapshot = await userRef.get();
+    
+    if(!snapshot.exists) {
+        const {displayName, email} = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            }) 
+        } catch (error) {
+            console.log('Error creating User', error.message);
+        }
+    }
+
+    return userRef;
 }
 
 firebase.initializeApp(config);
