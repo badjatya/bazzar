b0ed26a56a78dcbf20bb069efc88f81656ee56b9
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-04-15 18:06:07 +0530

    sign-in-and-sign-up styles

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -3,6 +3,8 @@ import React from 'react'
 import SignIn from '../../components/sign-in/sign-in.component'
 import SignUp from '../../components/sign-up/sign-up.component'
 
+import "./sign-in-and-sign-up.styles.scss"
+
 const SignInAndSignUpPage = () => (
     <div className="sign-in-and-sign-up">
         <SignIn />
