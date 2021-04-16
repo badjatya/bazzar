f3c974c8fcb01a274378a18225ab9632d45f0c04
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-04-16 23:47:08 +0530

    User Reducer switch

--- src/reducer/user/user.reducer.js
+++ src/reducer/user/user.reducer.js
@@ -1,3 +1,14 @@
 const INITIAL_STATE = {
     currentUser: null
 }
+
+const userReducer = (state = INITIAL_STATE, action) => {
+
+    switch (action.type) {
+        case '': 
+
+        default:
+            return state;
+    }
+
+}
