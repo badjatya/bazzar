f592333b06c51844add4272b2870aae27f33c589
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-04-17 22:47:13 +0530

    Store

--- src/reducer/store.js
+++ src/reducer/store.js
@@ -1,1 +1,8 @@
 import {createStore, applyMiddleware} from 'redux'
+import logger from 'redux-logger'
+
+import rootReducer from './root-reducer'
+
+const middleware = [logger];
+
+const store = createStore(rootReducer, applyMiddleware)
