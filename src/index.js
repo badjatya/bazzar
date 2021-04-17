6b4cad7d0faf929aa70934712d5553f62072e36f
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-04-17 23:03:23 +0530

    Added store In Provider (Redux)

--- src/index.js
+++ src/index.js
@@ -5,10 +5,11 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom"
 import { Provider } from 'react-redux'
+import store from './reducer/store'
 
 ReactDOM.render(
 
-  <Provider>
+  <Provider store={store}>
     <BrowserRouter >
       <App />
     </BrowserRouter>
