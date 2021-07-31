1e89ad6f0a679688d60a2e8aa7188ebfbd881a93
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-07-31 16:10:18 +0530

    update

--- src/index.js
+++ src/index.js
@@ -3,18 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from "react-router-dom"
-import { Provider } from 'react-redux'
-import store from './reducer/store'
 
 ReactDOM.render(
-
-  <Provider store={store}>
-    <BrowserRouter >
-      <App />
-    </BrowserRouter>
-  </Provider>
-  ,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
