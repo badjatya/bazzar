24dcb619ade62289d1ae3c5d584441d3aac79dfa
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-08-01 01:45:19 +0530

    added react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from "react";
 
+// Importing react router dom
+import { Switch, Route } from "react-router-dom";
+
 // Importing Pages
 import Homepage from "./App/pages/Homepage/Homepage";
 
 import "./App.css";
 
+const Hatspage = () => {
+  return <h1>Hats</h1>;
+};
+
 const App = () => {
   return (
-    <div className="App">
-      <Homepage />
-    </div>
+    <Switch>
+      <Route path="/" component={Homepage} exact />
+      <Route path="/hats" component={Hatspage} />
+    </Switch>
   );
 };
 
