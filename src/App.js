d10c5d7ad30bc9d7d9d114bb0e979b3f3e448bdc
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-08-01 16:15:54 +0530

    SignUp compoent created

--- src/App.js
+++ src/App.js
@@ -28,9 +28,21 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (user) => {
-      // this.setState({ currentUser: user });
-      createUserProfileDocument(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot((snapShot) => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data(),
+            },
+          });
+        });
+      }
+
+      this.setState({ currentUser: userAuth });
     });
   }
 
--- src/App/pages/Signin-signup/Signin-Signup.page.js
+++ src/App/pages/Signin-signup/Signin-Signup.page.js
@@ -2,14 +2,16 @@ import React from "react";
 
 // Importing Components
 import SignIn from "../../components/SignIn/SignIn.component";
+import SignUp from "../../components/SignUp/SignUp.component";
 
 // Importing Styles
 import "./signin-signup.styles.scss";
 
 const SignInAndSignUpPage = () => {
   return (
-    <div>
+    <div className="sign-in-and-sign-up">
       <SignIn />
+      <SignUp />
     </div>
   );
 };
