8d3b2dfd42af8af59f03eb84156d7e9849e00479
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-08-01 10:17:02 +0530

    Added collection preview and collection item and styled them in shop page

--- src/App/components/Collection-preview/CollectionPreview.component.js
+++ src/App/components/Collection-preview/CollectionPreview.component.js
@@ -1,5 +1,11 @@
 import React from "react";
 
+// Importing Components
+import CollectionItem from "./../Collection-item/CollectionItem.component";
+
+// Importing Styles
+import "./CollectionPreview.styles.scss";
+
 const CollectionPreview = ({ title, items }) => {
   return (
     <div className="collection-preview">
@@ -7,8 +13,8 @@ const CollectionPreview = ({ title, items }) => {
       <div className="preview">
         {items
           .filter((item, index) => index < 4)
-          .map((item) => (
-            <p key={item.id}>{item.name}</p>
+          .map(({ id, ...otherItemsProps }) => (
+            <CollectionItem key={id} {...otherItemsProps} />
           ))}
       </div>
     </div>
