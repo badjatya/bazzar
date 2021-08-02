d5d2e3b7aa0438a2a8b900662500617c485db98c
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-08-02 07:40:11 +0530

    added cart reducer, action and types

---
+++ src/App/redux/cart/cart.action.js
@@ -0,0 +1,6 @@
+// Importing Cart types
+import CartActionTypes from "./cart.types";
+
+export const toggleCartHidden = () => ({
+  type: CartActionTypes.TOGGLE_CART_HIDDEN,
+});
--- src/App/redux/root-reducer.js
+++ src/App/redux/root-reducer.js
@@ -3,7 +3,9 @@ import { combineReducers } from "redux";
 
 // Importing Reducers
 import userReducer from "./user/user.reducer";
+import cartReducer from "./cart/cart.reducer";
 
 export default combineReducers({
   user: userReducer,
+  cart: cartReducer,
 });
