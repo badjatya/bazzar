d9cea63dca8928f495350827710af2240b2b2014
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-08-02 07:30:42 +0530

    Added carticon component

---
+++ src/App/components/Shop-Icon/ShopIcon.component.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+// Importing redux
+import { connect } from "react-redux";
+import { toggleCartHidden } from "../../redux/cart/cart.actions";
+
+// Importing Icon
+import { ReactComponent as ShoppingIcon } from "../../assets/img/shopIcon.svg";
+
+// Importing Styles
+import "./ShopIcon.style.scss";
+
+const CartIcon = ({ toggleCartHidden }) => (
+  <div className="cart-icon" onClick={toggleCartHidden}>
+    <ShoppingIcon className="shopping-icon" />
+    <span className="item-count">0</span>
+  </div>
+);
+
+const mapDispatchToProps = (dispatch) => ({
+  toggleCartHidden: () => dispatch(toggleCartHidden()),
+});
+
+export default connect(null, mapDispatchToProps)(CartIcon);
