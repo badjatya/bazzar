a2c8f241b228153434285cdb62ca8f125517f38a
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-08-02 08:20:11 +0530

    added new state for cartItems in redux cart

--- src/App/redux/cart/cart.action.js
+++ src/App/redux/cart/cart.action.js
@@ -1,6 +1,13 @@
 // Importing Cart types
 import CartActionTypes from "./cart.types";
 
+// For toggling the dropdown
 export const toggleCartHidden = () => ({
   type: CartActionTypes.TOGGLE_CART_HIDDEN,
+});
+
+// For adding the items in cart
+export const addItem = (item) => ({
+  type: CartActionTypes.ADD_ITEM,
+  payload: item,
 });
