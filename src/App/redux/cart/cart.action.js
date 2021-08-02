fce543743785c2936dd07c3763ab390116b818b7
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-08-02 11:37:39 +0530

    removing items from cart

--- src/App/redux/cart/cart.action.js
+++ src/App/redux/cart/cart.action.js
@@ -1,13 +1,15 @@
-// Importing Cart types
 import CartActionTypes from "./cart.types";
 
-// For toggling the dropdown
 export const toggleCartHidden = () => ({
   type: CartActionTypes.TOGGLE_CART_HIDDEN,
 });
 
-// For adding the items in cart
 export const addItem = (item) => ({
   type: CartActionTypes.ADD_ITEM,
+  payload: item,
+});
+
+export const clearItemFromCart = (item) => ({
+  type: CartActionTypes.CLEAR_ITEM_FROM_CART,
   payload: item,
 });
