e59f0c4f9ee21d5a2e042197627b47a7e0ffd99d
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-08-02 08:45:48 +0530

    added cartitem in cartDropdown to show users all the items they have added in cart

--- src/App/components/Cart-Dropdown/CartDropdown.component.js
+++ src/App/components/Cart-Dropdown/CartDropdown.component.js
@@ -1,14 +1,27 @@
 import React from "react";
 
+// Importing Redux
+import { connect } from "react-redux";
+
+// Importing Components
 import CustomButton from "../Custom-Button/CustomButton.component";
+import CartItem from "../Cart-Item/CartItem.component";
 
 import "./CartDropdown.styles.scss";
 
-const CartDropdown = () => (
+const CartDropdown = ({ cartItems }) => (
   <div className="cart-dropdown">
-    <div className="cart-items" />
+    <div className="cart-items">
+      {cartItems.map((cartItem) => (
+        <CartItem key={cartItem.id} item={cartItem} />
+      ))}
+    </div>
     <CustomButton>GO TO CHECKOUT</CustomButton>
   </div>
 );
 
-export default CartDropdown;
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  cartItems,
+});
+
+export default connect(mapStateToProps)(CartDropdown);
